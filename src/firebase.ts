1d1107eaae2de25e4c1161e86f9059145176f579
Edgar Castro <edgar.castro.villa@outlook.com>
2021-12-03 17:58:37 -0500

    Updating secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,7 +9,6 @@ const {
 const getConfig: FirebaseOptions = {
   apiKey: process.env.REACT_APP_FB_API_KEY || 'localKey',
   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
   projectId: process.env.REACT_APP_FB_PROJECT_ID || 'test-venus',
   appId: process.env.REACT_APP_FB_APP_ID,
   measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
