30ae66783223751aa0ac38057d824f459f509510
Edgar Castro <edgar.castro.villa@outlook.com>
2021-12-14 14:13:04 -0500

    Update styles

--- src/components/FormQuestion/FormQuestion.tsx
+++ src/components/FormQuestion/FormQuestion.tsx
@@ -1,63 +1,68 @@
-import { Button, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
 import { collection, addDoc, CollectionReference } from "firebase/firestore";
-import { db } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
-import { QuestionType } from '../../utils/types';
+import { db } from "../../firebase";
+import { QuestionType } from "../../utils/types";
+import "./FormQuestion.scss";
 
 export const FormQuestion: React.FC = () => {
-  const [question, setQuestion] = useState('');
-  const [option1, setOption1] = useState('');
-  const [option2, setOption2] = useState('');
+  const [question, setQuestion] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
   const navigate = useNavigate();
 
-
-  const onClick = async () => {
-
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
-      const docRef = await addDoc<QuestionType>(collection(db, "questions") as CollectionReference<QuestionType>, {
-        questionDescription: question,
-        option1,
-        option2
-      });
+      const docRef = await addDoc<QuestionType>(
+        collection(db, "questions") as CollectionReference<QuestionType>,
+        {
+          questionDescription: question,
+          option1,
+          option2,
+        }
+      );
       console.log("Document written with ID: ", docRef.id);
       navigate(`/askme/${docRef.id}`);
-
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  }
-
+  };
 
   return (
-    <form>
+    <form className="FormQuestion" onSubmit={onSubmit} method="POST">
       <TextField
-        id="outlined-multiline-flexible"
-        label="Multiline"
+        id="input-question"
+        className="FormQuestion__input"
+        label="¿Cual es tu pregunta?"
         multiline
         maxRows={4}
         value={question}
-        onChange={e => setQuestion(e.target.value)}
+        onChange={(e) => setQuestion(e.target.value)}
       />
       <TextField
-        id="outlined-multiline-flexible"
-        label="Multiline"
+        id="input-option"
+        className="FormQuestion__input"
+        label="Ingresa la opcion"
         multiline
-        maxRows={4}
+        maxRows={2}
         value={option1}
-        onChange={e => setOption1(e.target.value)}
+        onChange={(e) => setOption1(e.target.value)}
       />
       <TextField
-        id="outlined-multiline-flexible"
-        label="Multiline"
+        id="input-option-unreachable"
+        className="FormQuestion__input"
+        label="Ingresa la opcion que no sera alcanzable"
         multiline
-        maxRows={4}
+        maxRows={2}
         value={option2}
-        onChange={e => setOption2(e.target.value)}
+        onChange={(e) => setOption2(e.target.value)}
       />
 
-      <Button onClick={onClick} variant="contained">Submit</Button>
+      <Button type="submit" variant="contained">
+        Enviar
+      </Button>
     </form>
-  )
+  );
 };
-
--- src/components/FunnyQuestion/FunnyQuestion.tsx
+++ src/components/FunnyQuestion/FunnyQuestion.tsx
@@ -1,60 +1,78 @@
-import { Button, Stack, Paper } from '@mui/material';
-import React, { useState } from 'react';
+import { Button, Stack, Paper } from "@mui/material";
+import React, { useState } from "react";
 
-import './FunnyQuestion.scss';
+import "./FunnyQuestion.scss";
 
 interface RequiredProps {
-    question: string;
-    option: string;
-    optionUnreachable: string;
+  question: string;
+  option: string;
+  optionUnreachable: string;
 }
 
 export type FunnyQuestionProps = RequiredProps;
 
 function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
 }
 
 function getRandomArbitrary(min: number, max: number) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
-export const FunnyQuestion: React.FC<FunnyQuestionProps> = props => {
-    const [isFirstMouseOver, setIsFirstMouseOver] = useState(false);
-    const [buttonPositionStyles, setButtonPositionStyles] = useState<React.CSSProperties>({});
+export const FunnyQuestion: React.FC<FunnyQuestionProps> = (props) => {
+  const [isFirstMouseOver, setIsFirstMouseOver] = useState(false);
+  const [buttonPositionStyles, setButtonPositionStyles] =
+    useState<React.CSSProperties>({});
 
-    const handleMouseOver = () => {
-        if (!isFirstMouseOver) {
-            setIsFirstMouseOver(true);
-        }
-
-        const { height, width } = getWindowDimensions();
-
-        const top = getRandomArbitrary(0, (height * 0.9));
-        const left = getRandomArbitrary(0, (width * 0.9));
-
-        setButtonPositionStyles({ position: 'absolute', left, top });
+  const handleMouseOver = () => {
+    if (!isFirstMouseOver) {
+      setIsFirstMouseOver(true);
     }
 
-    const handleRequiredClick = () => {
-        alert('Felicidades');
-    }
+    const { height, width } = getWindowDimensions();
 
-    const buttonUnrecheableStyles: React.CSSProperties = {
-        ...buttonPositionStyles,
-        position: isFirstMouseOver ? 'absolute' : 'initial',
-        transition: 'top 0.01s ease 0.01s, left 0.01s ease 0.01s'
-    }
+    const top = getRandomArbitrary(0, height * 0.9);
+    const left = getRandomArbitrary(0, width * 0.9);
 
-    return <Paper className="FunnyQuestion">
-        <h2>{props.question}</h2>
-        <Stack className="FunnyQuestion__options" spacing={2} direction="row" justifyContent="flex-start">
-            <Button onClick={handleRequiredClick} variant="contained">{props.option}</Button>
-            <Button tabIndex={-1} style={buttonUnrecheableStyles} onMouseOver={handleMouseOver} onTouchStart={handleMouseOver} variant="contained">{props.optionUnreachable}</Button>
-        </Stack>
+    setButtonPositionStyles({ position: "absolute", left, top });
+  };
+
+  const handleRequiredClick = () => {
+    alert("Felicidades");
+  };
+
+  const buttonUnrecheableStyles: React.CSSProperties = {
+    ...buttonPositionStyles,
+    position: isFirstMouseOver ? "absolute" : "initial",
+    transition: "top 0.01s ease 0.01s, left 0.01s ease 0.01s",
+  };
+
+  return (
+    <Paper className="FunnyQuestion">
+      <h2>{props.question}</h2>
+      <Stack
+        className="FunnyQuestion__options"
+        spacing={2}
+        direction="row"
+        justifyContent="flex-start"
+      >
+        <Button onClick={handleRequiredClick} variant="contained">
+          {props.option}
+        </Button>
+        <Button
+          tabIndex={-1}
+          style={buttonUnrecheableStyles}
+          onMouseOver={handleMouseOver}
+          onTouchStart={handleMouseOver}
+          variant="contained"
+        >
+          {props.optionUnreachable}
+        </Button>
+      </Stack>
     </Paper>
-}
+  );
+};
